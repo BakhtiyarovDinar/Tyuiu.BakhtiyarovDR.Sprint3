e763b9bda36aac81a66d142b79a0aee7e92cbd9b
Tyuiu.BakhtiyarovDR@outlook.com <dinar_bahtiyarov2005@mail.ru>
2023-11-04 16:55:16 +0500

    Task3

---
+++ Tyuiu.BakhtiyarovDR.Sprint3.Task3.V14.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BakhtiyarovDR.Sprint3.Task3.V14.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BakhtiyarovDR.Sprint3.Task3.V14.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6e63ea14-7e64-4c6d-92ea-3f0aa12d7214")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.BakhtiyarovDR.Sprint3.Task3.V14/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BakhtiyarovDR.Sprint3.Task3.V14.Lib;
+
+namespace Tyuiu.BakhtiyarovDR.Sprint3.Task3.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил Бахтияров Д. Р. | АСОиУБ-23-3 ";
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine("- Спринт #3                                                              -");
+            Console.WriteLine("- Тема: Оператор цикла foreach                                           -");
+            Console.WriteLine("- Задание #3                                                             -");
+            Console.WriteLine("- Вариант #14                                                            -");
+            Console.WriteLine("- Выполнил: Бахтияров Динар Русланович | АСОиУБ-23-3                     -");
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine("- УСЛОВИЕ:                                                               -");
+            Console.WriteLine("- Используя цикл foreach заменить буквы d  на ’ в строке: dgt ghedd bed  -");
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine("- ИСХОДНЫЕ ДАННЫЕ:                                                       -");
+            Console.WriteLine("--------------------------------------------------------------------------");
+
+            string value = "dgt ghedd bed";
+            char replacebale = 'd';
+            char replacement = '’';
+
+            Console.WriteLine("Исходная строка = " + value);
+            Console.WriteLine("Заменяемый символ = " + replacebale);
+            Console.WriteLine("Заменяющий символ = " + replacement);
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine("- РЕЗУЛЬТАТ:                                                             -");
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine("Получившаяся строка = " + ds.ReplaceCharInString(value, replacebale, replacement));
+            Console.ReadKey();
+        }
+    }
+}

087a50ae55211eb1bfb03743c28760d30d98b83b
Tyuiu.BakhtiyarovDR@outlook.com <dinar_bahtiyarov2005@mail.ru>
2023-11-04 15:46:40 +0500

    Task1

---
+++ Tyuiu.BakhtiyarovDR.Sprint3.Task1.V25.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BakhtiyarovDR.Sprint3.Task1.V25.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BakhtiyarovDR.Sprint3.Task1.V25.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7143d678-9bf4-4c8e-a0d0-2d240e241932")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
